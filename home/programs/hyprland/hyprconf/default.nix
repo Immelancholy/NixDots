80d5b1b2cbc2748c34ac069ab3ea0a969ae994ab
Immelancholy <lenalowes0@gmail.com>
2025-04-11 14:27:01 +0100

    Changes

--- home/programs/hyprland/hyprconf/default.nix
+++ home/programs/hyprland/hyprconf/default.nix
@@ -2,7 +2,6 @@
   wayland.windowManager.hyprland = {
     sourceFirst = true;
     settings = {
-      # source = ["/etc/xdg/hypr/hyprland.conf"];
       "$scr" = "$XDG_BIN_HOME";
       "$term" = "uwsm app -- kitty";
       "$files" = "uwsm app -- nemo.desktop";
