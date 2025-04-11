8804b38e79d3fdf1b7c9165132b9751255e1c8b0
Immelancholy <lenalowes0@gmail.com>
2025-04-11 20:30:08 +0100

    switch

--- system/boot.nix
+++ system/boot.nix
@@ -14,7 +14,7 @@
     bbswitch
     v4l2loopback
   ];
-  extraModprobeConfig = ''
+  boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
   # boot.kernelPackages = pkgs.linuxPackages_zen;
