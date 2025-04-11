7a12f8789bb47efece38a4d056ba8fc99aa865e1
Immelancholy <lenalowes0@gmail.com>
2025-04-11 14:03:20 +0100

    switch

--- flake.nix
+++ flake.nix
@@ -14,8 +14,7 @@
       flake = false;
     };
     home-manager = {
-      # url = "github:nix-community/home-manager";
-      url = "github:amadejkastelic/home-manager";
+      url = "github:nix-community/home-manager";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
