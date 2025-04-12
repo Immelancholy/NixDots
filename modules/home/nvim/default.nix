8de37be5368c60b4427b2cb6629939f306fccd70
Immelancholy <lenalowes0@gmail.com>
2025-04-12 22:18:05 +0100

    switch

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -30,8 +30,8 @@ in {
         nvn = "cd $FLAKE_PATH; nv";
       };
     };
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
+    # home.sessionVariables = {
+    #   EDITOR = "nvim";
+    # };
   };
 }
