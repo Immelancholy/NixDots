445239ec58c8ad1c1498746a934faf78218269e2
Immelancholy <lenalowes0@gmail.com>
2025-04-14 11:42:22 +0100

    switch

--- configuration.nix
+++ configuration.nix
@@ -111,20 +111,6 @@
       };
     };
     services.flatpak = {
-      overrides = {
-        global = {
-          Environment = {
-            XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
-          };
-        };
-      };
-      enable = true;
-      remotes = [
-        {
-          name = "flathub";
-          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        }
-      ];
       packages = [
         "com.github.PintaProject.Pinta"
       ];
