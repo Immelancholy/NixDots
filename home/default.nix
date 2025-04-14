13d7a89638e053b04e78a15edc14ef87883b042c
Immelancholy <lenalowes0@gmail.com>
2025-04-14 12:42:07 +0100

    switch

--- home/default.nix
+++ home/default.nix
@@ -57,6 +57,29 @@
       '';
     };
   };
+  home.file = {
+    ".vimium/Vimium-C-Binds.json".text = ''
+      {
+        "name": "Vimium C",
+        "@time": "14/04/2025, 12:38:22",
+        "time": 1744630702689,
+        "environment": {
+          "extension": "1.99.997",
+          "platform": "linux",
+          "firefox": 137
+        },
+        "keyLayout": 2,
+        "keyMappings": [
+          "#!no-check",
+          "unmap t",
+          "map J nextTab",
+          "map K previousTab",
+          ""
+        ],
+        "vimSync": true
+      }
+    '';
+  };
 
   home.stateVersion = "25.05";
   # Let home Manager install and manage itself.
