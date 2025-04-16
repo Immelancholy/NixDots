3f1e3e8a8470a56752b6016b07cb3b97ebdc0187
Immelancholy <lenalowes0@gmail.com>
2025-04-16 18:52:24 +0100

    switch

--- home/programs/homeActivation.nix
+++ home/programs/homeActivation.nix
@@ -4,43 +4,42 @@
   ...
 }: let
   cavaCfg = pkgs.writeShellScriptBin "cavaCfg" ''
-        cavaConfigFile=$HOME/.config/cava/vcConfig
-        id=$(${pkgs.wireplumber}/bin/wpctl status | grep "virtual_cable_in" | ${pkgs.gawk}/bin/awk '{print $2}' | grep -m1 "" | cut -f1 -d ".")
-        if ! [[ -f "$cavaConfigFile" ]] ; then
-          cat >"$cavaConfigFile" <<EOF
-          [color]
-          gradient=1
-          gradient_color_1='#94e2d5'
-          gradient_color_2='#89dceb'
-          gradient_color_3='#74c7ec'
-          gradient_color_4='#89b4fa'
-          gradient_color_5='#cba6f7'
-          gradient_color_6='#f5c2e7'
-          gradient_color_7='#eba0ac'
-          gradient_color_8='#f38ba8'
+    cavaConfigFile=$HOME/.config/cava/vcConfig
+    id=$(${pkgs.wireplumber}/bin/wpctl status | grep "virtual_cable_in" | ${pkgs.gawk}/bin/awk '{print $2}' | grep -m1 "" | cut -f1 -d ".")
 
-          [general]
-          bar_spacing=0
-          bar_width=1
-          bars=0
-          framerate=144
-          sensitivity=100
+    cat >"$cavaConfigFile" <<EOF
+    [color]
+    gradient=1
+    gradient_color_1='#94e2d5'
+    gradient_color_2='#89dceb'
+    gradient_color_3='#74c7ec'
+    gradient_color_4='#89b4fa'
+    gradient_color_5='#cba6f7'
+    gradient_color_6='#f5c2e7'
+    gradient_color_7='#eba0ac'
+    gradient_color_8='#f38ba8'
 
-          [input]
-          method=pipewire
-          source=''${id}
+    [general]
+    bar_spacing=0
+    bar_width=1
+    bars=0
+    framerate=144
+    sensitivity=100
 
-          [output]
-          channels=stereo
-          method=noncurses
-          mono_option=average
+    [input]
+    method=pipewire
+    source=''${id}
 
-          [smoothing]
-          monstercat=1
-          noise_reduction=72
-          waves=0
+    [output]
+    channels=stereo
+    method=noncurses
+    mono_option=average
+
+    [smoothing]
+    monstercat=1
+    noise_reduction=72
+    waves=0
     EOF
-        fi
   '';
 in {
   home.activation = {
