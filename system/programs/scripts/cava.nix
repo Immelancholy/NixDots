a5d448aa2058b68ce58758ebdab856beaaf6c039
Immelancholy <lenalowes0@gmail.com>
2025-04-16 12:47:18 +0100

    switch

--- system/programs/scripts/cava.nix
+++ system/programs/scripts/cava.nix
@@ -3,7 +3,7 @@
     (pkgs.writeShellScriptBin "cava.sh" ''
       kitty @ set-spacing padding=0
       kitty @ set-font-size 3
-      cava "$@"
+      cava -p $XDG_CONFIG_HOME/cava/vcConfig "$@"
       kitty @ set-spacing padding=default
       kitty @ set-font-size 10
     '')
