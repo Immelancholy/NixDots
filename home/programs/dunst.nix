722894102a43db91ef7f543fdecae6009b2bc4df
Immelancholy <lenalowes0@gmail.com>
2025-04-16 16:16:34 +0100

    switch

--- home/programs/dunst.nix
+++ home/programs/dunst.nix
@@ -10,16 +10,14 @@
         highlight = "#89b4fac0";
         frame_width = 2;
         corner_radius = 15;
-        padding = 2;
-        horizontal_padding = 2;
         origin = "top-right";
         offset = "(54, 18)";
         width = "(0, 400)";
         alignment = "center";
         icon_corner_radius = 15;
         icon_position = "top";
-        max_icon_size = 256;
-        font = "JetBrainsMono Nerd Font Mono 10";
+        max_icon_size = 128;
+        font = "JetBrainsMono Nerd Font Mono 9";
       };
       urgency_low = {
         background = "#1e1e2e99";
