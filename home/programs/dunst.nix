9f890a58f596b015a2f8bc805742397d0fea4d2d
Immelancholy <lenalowes0@gmail.com>
2025-04-16 16:55:36 +0100

    switch

--- home/programs/dunst.nix
+++ home/programs/dunst.nix
@@ -13,12 +13,13 @@
         corner_radius = 15;
         origin = "top-right";
         offset = "(54, 18)";
-        width = "(0, 300)";
-        height = "(0, 400)";
+        width = "(0, 280)";
+        height = "(0, 300)";
         alignment = "center";
         icon_corner_radius = 15;
         icon_position = "top";
-        padding = 15;
+        padding = 10;
+        horizontal_padding = 10;
         max_icon_size = 256;
         font = "JetBrainsMono Nerd Font Mono 9";
       };
