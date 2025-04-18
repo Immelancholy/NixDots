39af2a1134db54855b4cb6c9505cfa967eba8f54
Immelancholy <lenalowes0@gmail.com>
2025-04-18 16:54:48 +0100

    switch

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -15,9 +15,9 @@
         "Alt, Return, fullscreen"
         ''$mods, U, exec, [workspace 1 silent; float; size 858 559; move 640 40] $term --class "rmpc" uwsm app -- rmpc.sh''
         ''$mods, U, exec, [workspace 1 silent; float; size 858 462; move 640 609] $term --class "cava" uwsm app -- cava.sh''
-        ''$mods, U, exec, [workspace 1 silent; float; size 620 666; move 10 404] $term --class "btop" uwsm app -- btop.sh''
+        ''$mods, U, exec, [workspace 1 silent; float; size 620 637; move 10 433] $term --class "btop" uwsm app -- btop.sh''
         ''$mods, U, exec, [workspace 1 silent; float; size 402 1030; move 1508 40] $term --class "neo" uwsm app -- neo.sh''
-        ''$mods, U, exec, [workspace 1 silent; float; size 620 354; move 10 40] $term --class "fastfetch" kitty @ launch --type overlay --env class="fastfetch"''
+        ''$mods, U, exec, [workspace 1 silent; float; size 620 383; move 10 40] $term --class "fastfetch" kitty @ launch --type overlay --env class="fastfetch"''
         "$mod, Delete, exec, rofi -show power-menu -modi power-menu:rofi-power-menu"
         "$mod, 0, workspace, 10"
         "Ctrl+Alt, L, exec, uwsm-app -- swaylock -fF"
--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -6,9 +6,9 @@
       "sleep 10; systemctl restart --user remmina"
       ''[workspace 1 silent; float; size 858 559; move 640 40] $term --class "rmpc" uwsm app -- rmpc.sh''
       ''[workspace 1 silent; float; size 858 462; move 640 609] $term --class "cava" uwsm app -- cava.sh''
-      ''[workspace 1 silent; float; size 620 666; move 10 404] $term --class "btop" uwsm app -- btop.sh''
+      ''[workspace 1 silent; float; size 620 637; move 10 433] $term --class "btop" uwsm app -- btop.sh''
       ''[workspace 1 silent; float; size 402 1030; move 1508 40] $term --class "neo" uwsm app -- neo.sh''
-      ''[workspace 1 silent; float; size 620 354; move 10 40] $term --class "fastfetch" kitty @ launch --type overlay --env class="fastfetch"''
+      ''[workspace 1 silent; float; size 620 383; move 10 40] $term --class "fastfetch" kitty @ launch --type overlay --env class="fastfetch"''
       "[workspace 2 silent] $browser"
       "[workspace 2 silent] uwsm app -- teams-for-linux --minimized"
       "uwsm app -- $discord"
