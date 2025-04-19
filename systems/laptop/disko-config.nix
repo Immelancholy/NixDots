1dbd74c5983dd4817cc1ec9ebedd7b1de60a9d5e
Immelancholy <lenalowes0@gmail.com>
2025-04-19 14:36:43 +0100

    trying nixos-anywhere

--- systems/laptop/disko-config.nix
+++ systems/laptop/disko-config.nix
@@ -13,6 +13,7 @@
           type = "gpt";
           partitions = {
             boot = {
+              priority = 1;
               size = "512M";
               type = "EF00";
               content = {
