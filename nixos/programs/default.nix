f59a638618c1d7f9e414090de53b4b120d569ad1
Immelancholy <lenalowes0@gmail.com>
2025-04-20 12:34:09 +0100

    yeet

--- nixos/programs/default.nix
+++ nixos/programs/default.nix
@@ -89,6 +89,97 @@
     wine-wayland
     xdg-user-dirs
     xdg-user-dirs-gtk
+    ghostscript
+    tectonic
+    mermaid-cli
+    python313Packages.pylatexenc
+    vesktop
+    grim
+    slurp
+    pipes
+    lazygit
+    loupe
+    glycin-loaders
+    swappy
+    libsixel
+
+    manix
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+    ventoy
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+    neo
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+    kdePackages.qtstyleplugin-kvantum
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    rmpc
+    protonup
+    playerctl
+    pokemonsay
+    fortune
+    cowsay
+    lz4
+    kdePackages.ark
+    parted
+    # retroarch-free
+    qalculate-gtk
+    libreoffice
+    fd
+    wordnet
   ];
   programs.ssh = {
     startAgent = false;
