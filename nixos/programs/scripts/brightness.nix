f5fb6cdb8b428f5edd19270fd701224b9324152c
Immelancholy <lenalowes0@gmail.com>
2025-04-20 22:55:36 +0100

    switch

--- nixos/programs/scripts/brightness.nix
+++ nixos/programs/scripts/brightness.nix
@@ -8,7 +8,7 @@
 
         BRIGHTNESS_PERCENT=$(${pkgs.bc}/bin/bc <<< "scale=1; "$CURRENT_BRIGHTNESS" / "$MAX_BRIGHTNESS" * 100")
 
-        dunstctl close
+        dunstctl close-all
         dunstify -t 3000 -a "  Brightness" -h int:value:"$BRIGHTNESS_PERCENT" "$BRIGHTNESS_PERCENT""%"
       }
 
