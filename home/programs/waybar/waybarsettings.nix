e37fbc7b8df67ae61d86c8747f2e4e69b8094cbe
Immelancholy <lenalowes0@gmail.com>
2025-04-21 22:38:55 +0100

    switch

--- home/programs/waybar/waybarsettings.nix
+++ home/programs/waybar/waybarsettings.nix
@@ -42,6 +42,8 @@
         "custom/r_end"
         "custom/l_end"
         "tray"
+        "custom/r_end"
+        "custom/l_end"
         "custom/power"
         "custom/r_end"
         "custom/padd"
@@ -184,7 +186,7 @@
         tooltip-format = "{format_source} {source_desc} // {source_volume}%";
       };
       "custom/power" = {
-        format = "⏻";
+        format = "";
         tooltip = false;
         on-click = "rofi -show power-menu -modi power-menu:rofi-power-menu ";
       };
