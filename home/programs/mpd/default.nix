d045b7084fea0b0010036148bbaa1a6d01deb01e
Immelancholy <lenalowes0@gmail.com>
2025-04-22 12:15:41 +0100

    switch

--- home/programs/mpd/default.nix
+++ home/programs/mpd/default.nix
@@ -14,6 +14,7 @@ in {
         startWhenNeeded = true;
       };
       musicDirectory = "/home/${user}/Music";
+      dataDir = "/home/${user}/.mpd";
       extraConfig = ''
         restore_paused "yes"
 
--- modules/system/users/sudoUsers.nix
+++ modules/system/users/sudoUsers.nix
@@ -25,7 +25,7 @@ in {
         lib.nameValuePair user {
           isNormalUser = true;
           description = "Account for ${user}";
-          extraGroups = ["networkmanager" "wheel" "video" "seat"];
+          extraGroups = ["mpd" "networkmanager" "wheel" "video" "seat"];
           shell = pkgs.zsh;
           initialPassword = "password";
         })
