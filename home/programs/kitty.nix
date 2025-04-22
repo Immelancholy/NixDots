3cc5b911ef8adbb2edec03253816889a65a32312
Immelancholy <lenalowes0@gmail.com>
2025-04-22 10:36:48 +0100

    switch

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -11,7 +11,7 @@
       window_padding_width = 5;
       allow_remote_control = true;
       # listen_on = "unix:/run/user/1000/mykitty";
-      listen_on = "tcp:127.0.0.1:20000";
+      listen_on = "unix:/tmp/mykitty";
     };
     extraConfig = ''
       # background_opacity 0.67
