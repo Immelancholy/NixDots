5c03ba2f0c9280c35a96a1576d485600176c6bc4
Immelancholy <lenalowes0@gmail.com>
2025-04-22 15:25:18 +0100

    switch

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -13,7 +13,7 @@
         "$mod, grave, exec, $menu"
         "$mod, C, exec, $edit"
         "Alt, Return, fullscreen"
-        ''$mods, U, exec, [workspace 1 silent; float; size 858 559; move 640 40] $term --class "inori" uwsm app -- inori''
+        ''$mods, U, exec, [workspace 1 silent; float; size 348 559; move 640 40] $term --class "inori" uwsm app -- inori''
         ''$mods, U, exec, [workspace 1 silent; float; size 858 462; move 640 609] $term --class "cava" uwsm app -- cava.sh''
         ''$mods, U, exec, [workspace 1 silent; float; size 620 637; move 10 433] $term --class "btop" uwsm app -- btop.sh''
         ''$mods, U, exec, [workspace 1 silent; float; size 402 1030; move 1508 40] $term --class "neo" uwsm app -- neo.sh''
--- home/programs/mpd/client/inori.nix
+++ home/programs/mpd/client/inori.nix
@@ -11,5 +11,17 @@
 
     [theme.item_highlight_inactive]
     fg = "#cdd6f4"
+
+    [theme.block_active]
+    fg = "#cba6f7"
+
+    [theme.playing]
+    fg = "#cba6f7"
+
+    [theme.status_artist]
+    fg = "#89dceb"
+
+    [theme.status_album]
+    fg = "#cba6f7"
   '';
 }
