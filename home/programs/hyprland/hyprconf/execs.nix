8a6c043a0ecdd5f5e45d347233d07fe66e704e63
Immelancholy <lenalowes0@gmail.com>
2025-04-22 14:27:34 +0100

    switch

--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -4,7 +4,7 @@
       "hyprctl setcursor Bibata-Modern-Ice 22"
       "systemctl restart --user nm-applet"
       "sleep 10; systemctl restart --user remmina"
-      ''[workspace 1 silent; float; size 858 559; move 640 40] $term --class "rmpc" uwsm app -- rmpc.sh''
+      ''[workspace 1 silent; float; size 858 559; move 640 40] $term --class "ncmpcpp" uwsm app -- ncmpcpp''
       ''[workspace 1 silent; float; size 858 462; move 640 609] $term --class "cava" uwsm app -- cava.sh''
       ''[workspace 1 silent; float; size 620 637; move 10 433] $term --class "btop" uwsm app -- btop.sh''
       ''[workspace 1 silent; float; size 402 1030; move 1508 40] $term --class "neo" uwsm app -- neo.sh''
