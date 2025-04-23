a8446471e2f11a2ac339f7117732648366d08366
Immelancholy <lenalowes0@gmail.com>
2025-04-23 21:04:37 +0100

    switch

--- modules/home/stylix/spicetify.nix
+++ modules/home/stylix/spicetify.nix
@@ -1,10 +1,12 @@
 {
   config,
   lib,
+  inputs,
   ...
 }:
 with lib; let
   cfg = config.stylix.targets.spicetify;
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in {
   config = mkIf (!cfg.enable) {
     programs.spicetify = {
