928051027785b08550e43a79f8c4f2edcaab7a77
Immelancholy <lenalowes0@gmail.com>
2025-04-23 11:05:03 +0100

    switch

--- flake.nix
+++ flake.nix
@@ -164,7 +164,7 @@
           nix-flatpak.nixosModules.nix-flatpak
           solaar.nixosModules.default
           catppuccin.nixosModules.catppuccin
-          ./nixos
+          ./nixos/system
           ./modules/system
           ./hosts/laptop
 
