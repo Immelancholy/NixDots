c3a5eaeff25af1b39ffebf17bbfd4ac27b147697
Immelancholy <lenalowes0@gmail.com>
2025-04-23 10:25:32 +0100

    switch

--- nixos/programs/scripts/volume.nix
+++ nixos/programs/scripts/volume.nix
@@ -9,27 +9,27 @@
 
         dunstctl close-all
         dunstify -t 3000 -a "  Volume" -h int:value:"$VOLUME_PERCENT" "$VOLUME_PERCENT""%"
-
-
-        if [[ "$#" != 1 || ! ("$1" == "inc" || "$1" == "dec") ]]; then
-            printf "Usage: %s [inc|dec]\n" "$0" >&2
-            exit 1
-        fi
-
-        # Check if brightnessctl is available
-        if ! command -v mpc &> /dev/null; then
-          echo "Error: mpc is not installed. Please install it." >&2
-          exit 1
-        fi
-
-        if [[ "$1" == "inc" ]]; then
-          mpc volume +1 > /dev/null
-          notify_volume
-        elif [[ "$1" == "dec" ]]; then
-          mpc volume -1 > /dev/null
-          notify_volume
-        fi
       }
+
+
+      if [[ "$#" != 1 || ! ("$1" == "inc" || "$1" == "dec") ]]; then
+          printf "Usage: %s [inc|dec]\n" "$0" >&2
+          exit 1
+      fi
+
+      # Check if brightnessctl is available
+      if ! command -v mpc &> /dev/null; then
+        echo "Error: mpc is not installed. Please install it." >&2
+        exit 1
+      fi
+
+      if [[ "$1" == "inc" ]]; then
+        mpc volume +1 > /dev/null
+        notify_volume
+      elif [[ "$1" == "dec" ]]; then
+        mpc volume -1 > /dev/null
+        notify_volume
+      fi
     '')
   ];
 }
