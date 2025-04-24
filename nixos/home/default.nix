8266b52e3a11df9f1e5dbd95a8a6894f17876e77
Immelancholy <lenalowes0@gmail.com>
2025-04-24 05:36:11 +0100

    switch

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -37,19 +37,19 @@ in {
       "autostart/input-mapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
         source = "${nixosConfig.services.input-remapper.package}/share/applications/input-remapper-autoload.desktop";
       };
+      "neo/colors".text = ''
+        neo_color_version 1
+        -1
+        177
+        141
+        183
+        153
+        224
+        225
+      '';
     };
   };
   home.file = {
-    "neo/colors".text = ''
-      neo_color_version 1
-      -1
-      177
-      141
-      183
-      153
-      224
-      225
-    '';
     ".vimium/Vimium-C-Binds.json".text = ''
       {
         "name": "Vimium C",
