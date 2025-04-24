a2550d3fa98ebf55ab2d46456978e8e8c0545baa
Immelancholy <lenalowes0@gmail.com>
2025-04-24 05:23:26 +0100

    switch

--- nixos/system/programs/scripts/pipes.nix
+++ nixos/system/programs/scripts/pipes.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "pipes" ''
       kitty @ set-spacing padding=2
-      pipes.sh -p 8 -t 1 -r 3500 "$@"
+      pipes.sh -p 8 -t 1 -r 0 "$@"
       kitty @ set-spacing padding=default
     '')
   ];
