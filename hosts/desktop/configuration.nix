b15119f43ed55539f7fca9ebc0c5dc2046b0aa7c
Immelancholy <lenalowes0@gmail.com>
2025-04-25 21:08:13 +0100

    switch

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -18,7 +18,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
 
-    image = ../../nixos/home/backgrounds/Evil_Miku.mp4;
+    image = ../../nixos/home/backgrounds/Evil_Miku.png;
   };
 
   services.openssh.enable = true;
