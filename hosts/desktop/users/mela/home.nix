43c5218adba00123c1301542be7858c999bde053
Immelancholy <lenalowes0@gmail.com>
2025-04-26 01:27:26 +0100

    switch

--- hosts/desktop/users/mela/home.nix
+++ hosts/desktop/users/mela/home.nix
@@ -48,7 +48,7 @@
       useHyprspace = false;
       liveWallpaper = {
         enable = true;
-        path = ../../../../nixos/home/backgrounds/Smoke_In_Rain.mp4;
+        path = ../../../../nixos/home/backgrounds/Momo_Smoke.mp4;
       };
       settings = {
         cursor = {
