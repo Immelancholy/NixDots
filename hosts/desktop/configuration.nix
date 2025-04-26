6500c9e3c10fc0fb61084d0d71fb10b2ede9eed5
Immelancholy <lenalowes0@gmail.com>
2025-04-26 02:05:41 +0100

    switch

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -18,7 +18,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
 
-    image = ../../nixos/home/backgrounds/Evil_Miku.png;
+    image = ../../nixos/home/backgrounds/Arcueid.png;
   };
 
   services.openssh.enable = true;
@@ -78,7 +78,7 @@
       enable = true;
       animatedBackground = {
         enable = true;
-        path = ../../nixos/home/backgrounds/Evil_Miku.mp4;
+        path = ../../nixos/home/backgrounds/Arcueid.mp4;
       };
     };
     tuiGreet.enable = false;
