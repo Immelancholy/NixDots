7906dccc97483406925171c90383db2da08997d8
Immelancholy <lenalowes0@gmail.com>
2025-05-02 19:10:17 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -1,42 +1,17 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "mpdart" ''
-      function print_centered() {
-           [[ $# == 0 ]] && return 1
-
-           declare -i TERM_COLS="$(tput cols)"
-           declare -i str_len="''${#1}"
-           [[ $str_len -ge $TERM_COLS ]] && {
-                echo "$1";
-                return 0;
-           }
-
-           declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
-           [[ $# -ge 2 ]] && ch="''${2:0:1}" || ch=" "
-           filler=""
-           for (( i = 0; i < filler_len; i++ )); do
-                filler="''${filler}''${ch}"
-           done
-
-           printf "%s%s%s" "$filler" "$1" "$filler"
-           [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "''${ch}"
-           printf "\n"
-
-           return 0
-      }
       get_art () {
         song=$(mpc current --format %file%)
-        art=$(playerctl --player=mpd metadata --format "{{mpris:artUrl}}")
         if [ "$song" != "$song_old" ]; then
           clear
-          kitten icat --align=center "$art"
+          art=$(playerctl --player=mpd metadata --format "{{mpris:artUrl}}")
           title=$(mpc current --format %title%)
           artist=$(mpc current --format %artist%)
-          columns=$(tput cols)
           line1="-------- $artist --------"
           line2="-------- $title --------"
-          # print_centered "$line1" " "
-          # print_centered "$line2" " "
+          kitten icat --align=center "$art"
+          columns=$(tput cols)
           printf "%*s\n" $(($columns/2)) "$line1"
           columns=$(tput cols)
           printf "%*s\n" $(($columns/2)) "$line2"
