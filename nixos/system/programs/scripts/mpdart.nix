f9f5c58900747670902fcf43e35f31d126e14416
Immelancholy <lenalowes0@gmail.com>
2025-05-02 19:06:38 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -2,43 +2,27 @@
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "mpdart" ''
       function print_centered() {
-           # [[ $# == 0 ]] && return 1
-           #
-           # declare -i TERM_COLS="$(tput cols)"
-           # declare -i str_len="''${#1}"
-           # [[ $str_len -ge $TERM_COLS ]] && {
-           #      echo "$1";
-           #      return 0;
-           # }
-           #
-           # declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
-           # [[ $# -ge 2 ]] && ch="''${2:0:1}" || ch=" "
-           # filler=""
-           # for (( i = 0; i < filler_len; i++ )); do
-           #      filler="''${filler}''${ch}"
-           # done
-           #
-           # printf "%s%s%s" "$filler" "$1" "$filler"
-           # [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "''${ch}"
-           # printf "\n"
-           #
-           # return 0
-        local heading width headinglen padlength i
-        heading="$*"
-        width="$(tput cols)"
-        headinglen=''${#heading}
-        padlength=$(( (width - 2 - headinglen) / 2 ))
-        for ((i = 0; i < padlength; ++i)); do
-          printf ""
-        done
-        printf " %s " "$heading"
-        for ((i = 0; i < padlength; ++i)); do
-          printf ""
-        done
-        if (( (width - 2 - headinglen) % 2 )); then
-          printf ""
-        fi
-        printf "\n"
+           [[ $# == 0 ]] && return 1
+
+           declare -i TERM_COLS="$(tput cols)"
+           declare -i str_len="''${#1}"
+           [[ $str_len -ge $TERM_COLS ]] && {
+                echo "$1";
+                return 0;
+           }
+
+           declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
+           [[ $# -ge 2 ]] && ch="''${2:0:1}" || ch=" "
+           filler=""
+           for (( i = 0; i < filler_len; i++ )); do
+                filler="''${filler}''${ch}"
+           done
+
+           printf "%s%s%s" "$filler" "$1" "$filler"
+           [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "''${ch}"
+           printf "\n"
+
+           return 0
       }
       get_art () {
         song=$(mpc current --format %file%)
@@ -48,10 +32,14 @@
           kitten icat --align=center "$art"
           title=$(mpc current --format %title%)
           artist=$(mpc current --format %artist%)
+          columns=$(tput cols)
           line1="-------- $artist --------"
           line2="-------- $title --------"
-          print_centered "$line1" " "
-          print_centered "$line2" " "
+          # print_centered "$line1" " "
+          # print_centered "$line2" " "
+          printf "%*s\n" $(($columns/2)) "$line1"
+          columns=$(tput cols)
+          printf "%*s\n" $(($columns/2)) "$line2"
           song_old=$song
         fi
       }
