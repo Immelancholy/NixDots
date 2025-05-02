b75454da72b9a3e1c6532f69506b9398d0d635b9
Immelancholy <lenalowes0@gmail.com>
2025-05-02 12:24:09 +0100

    switch

--- nixos/home/programs/hyprland/default.nix
+++ nixos/home/programs/hyprland/default.nix
@@ -14,8 +14,9 @@
       variables = ["--all"];
     };
     plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
-      inputs.hyprland-plugins.packages.${pkgs.system}.xtra-dispatchers
+      # inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
+      # inputs.hyprland-plugins.packages.${pkgs.system}.xtra-dispatchers
+      pkgs.hyprlandPlugins.csgo-vulkan-fix
     ];
   };
   services.hyprpolkitagent.enable = true;
