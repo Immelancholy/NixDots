988d76709d6aa08f2f38b317df4d9006a51f224a
Immelancholy <lenalowes0@gmail.com>
2025-05-02 19:01:32 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -1,28 +1,44 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "mpdart" ''
-      function print_centered {
-           [[ $# == 0 ]] && return 1
-
-           declare -i TERM_COLS="$(tput cols)"
-           declare -i str_len="''${#1}"
-           [[ $str_len -ge $TERM_COLS ]] && {
-                echo "$1";
-                return 0;
-           }
-
-           declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
-           [[ $# -ge 2 ]] && ch="''${2:0:1}" || ch=" "
-           filler=""
-           for (( i = 0; i < filler_len; i++ )); do
-                filler="''${filler}''${ch}"
-           done
-
-           printf "%s%s%s" "$filler" "$1" "$filler"
-           [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "''${ch}"
-           printf "\n"
-
-           return 0
+      function print_centered() {
+           # [[ $# == 0 ]] && return 1
+           #
+           # declare -i TERM_COLS="$(tput cols)"
+           # declare -i str_len="''${#1}"
+           # [[ $str_len -ge $TERM_COLS ]] && {
+           #      echo "$1";
+           #      return 0;
+           # }
+           #
+           # declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
+           # [[ $# -ge 2 ]] && ch="''${2:0:1}" || ch=" "
+           # filler=""
+           # for (( i = 0; i < filler_len; i++ )); do
+           #      filler="''${filler}''${ch}"
+           # done
+           #
+           # printf "%s%s%s" "$filler" "$1" "$filler"
+           # [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "''${ch}"
+           # printf "\n"
+           #
+           # return 0
+        local heading width headinglen padlength i
+        heading="$*"
+        width="$(tput cols)"
+        headinglen=''${#heading}
+        padlength=$(( (width - 2 - headinglen) / 2 ))
+        for ((i = 0; i < padlength; ++i)); do
+          printf "-"
+        done
+        printf " %s " "$heading"
+        for ((i = 0; i < padlength; ++i)); do
+          printf "-"
+        done
+        if (( (width - 2 - headinglen) % 2 )); then
+          printf "-"
+        fi
+        printf "\n"
       }
       get_art () {
         song=$(mpc current --format %file%)
