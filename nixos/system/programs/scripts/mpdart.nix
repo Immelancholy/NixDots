2e5fd6d4ef6130c47964002dde17679acb293ba8
Immelancholy <lenalowes0@gmail.com>
2025-05-03 19:05:56 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -8,7 +8,52 @@
       source "${inputs.bash-utility}/src/format.sh"
       TMP="/tmp"
       COVER="$TMP/albumArt.jpg"
-      MUSIC_DIR="~/Music"
+      MUSIC_DIR="''${MPD_MUSIC_DIR:-$HOME/Music}"
+      PROG_NAME=$(basename "$0")
+
+      show_help() {
+          printf "%s" "\
+      usage: mpdart [-h|--help] [--music_dir \"path/to/dir\"] [--verbose]
+
+      optional arguments:
+         -h, --help            show this help message and exit
+         --music_dir           the music directory which MPD plays from
+         --verbose             show the output
+      "
+      }
+
+      log() {
+          [ "$VERBOSE" ] && echo "$PROG_NAME: $1"
+      }
+
+      logError() {
+          echo "$PROG_NAME: $1" >&2
+          exit 1
+      }
+
+      while true; do
+          case "$1" in
+              --music_dir)
+                  shift;
+                  MUSIC_DIR=$1
+                  ;;
+              -h|--help)
+                  show_help
+                  exit
+                  ;;
+              --verbose)
+                  VERBOSE=true
+                  ;;
+              --)
+                  shift
+                  break
+                  ;;
+          esac
+          shift
+      done
+
+      options=$(getopt -o h --long 'music_dir:,verbose,help' -- "$@")
+      eval set -- "$options"
 
       get_art () {
         song=$(mpc current --format %file%)
