95e47bd58ad3e9be52dfd22ce944944ad7ed9693
Immelancholy <lenalowes0@gmail.com>
2025-05-03 20:03:13 +0100

    switch

--- modules/system/services/mpdchck.nix
+++ modules/system/services/mpdchck.nix
@@ -6,6 +6,80 @@
 }:
 with lib; let
   cfg = config.services.mpdchck;
+
+  mpdchck = pkgs.writeShellScriptBin "mpdchck.sh" ''
+    while :
+    do
+      state=$(mpc status %state%)
+      if [ "$state" = "playing" ];
+      then
+        sr.sh > /dev/null
+      fi
+    done
+  '';
+  srsh = pkgs.writeShellScriptBin "sr.sh" ''
+    pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+    mapfile -t allowedRates < <( pw-metadata -n settings | grep 'clock.allowed-rates' | cut -d "'" -f 4 | tr -d "[" | tr -d "]" | tr -d , )
+    while :
+    do
+      state=$(mpc status %state%)
+      if [ "$state" != "playing" ];
+      then
+        pw-metadata -n settings 0 clock.force-rate 0 > /dev/null
+
+        break
+      else
+        currentRate=$(mpc status %samplerate%)
+
+        if [ "$currentRate" != "$pwrate" ];
+        then
+          if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
+          then
+            pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
+            pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+
+          elif [ $(( currentRate % 48000 )) -eq 0 ];
+          then
+            if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
+            then
+              pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
+              pwrate=''${currentRate}
+
+            elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
+            then
+              pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
+              pwrate=''${currentRate}
+
+            elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
+            then
+              pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
+              pwrate=''${currentRate}
+
+            else
+              kill "$pid"
+              pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+              pwrate=''${currentRate}
+
+            fi
+
+          else
+            if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
+            then
+              pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
+              pwrate=''${currentRate}
+
+            else
+              pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+              pwrate=''${currentRate}
+
+            fi
+          fi
+
+        fi
+      fi
+
+    done
+  '';
 in {
   options.services.mpdchck = {
     enable = mkEnableOption "Enable mpdchck service";
@@ -22,79 +96,8 @@ in {
   };
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      (pkgs.writeShellScriptBin "mpdchck.sh" ''
-        while :
-        do
-          state=$(mpc status %state%)
-          if [ "$state" = "playing" ];
-          then
-            sr.sh > /dev/null
-          fi
-        done
-      '')
-      (pkgs.writeShellScriptBin "sr.sh" ''
-        pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
-        mapfile -t allowedRates < <( pw-metadata -n settings | grep 'clock.allowed-rates' | cut -d "'" -f 4 | tr -d "[" | tr -d "]" | tr -d , )
-        while :
-        do
-          state=$(mpc status %state%)
-          if [ "$state" != "playing" ];
-          then
-            pw-metadata -n settings 0 clock.force-rate 0 > /dev/null
-
-            break
-          else
-            currentRate=$(mpc status %samplerate%)
-
-            if [ "$currentRate" != "$pwrate" ];
-            then
-              if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
-              then
-                pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
-                pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
-
-              elif [ $(( currentRate % 48000 )) -eq 0 ];
-              then
-                if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
-                then
-                  pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
-                  pwrate=''${currentRate}
-
-                elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
-                then
-                  pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
-                  pwrate=''${currentRate}
-
-                elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
-                then
-                  pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
-                  pwrate=''${currentRate}
-
-                else
-                  kill "$pid"
-                  pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-                  pwrate=''${currentRate}
-
-                fi
-
-              else
-                if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
-                then
-                  pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
-                  pwrate=''${currentRate}
-
-                else
-                  pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-                  pwrate=''${currentRate}
-
-                fi
-              fi
-
-            fi
-          fi
-
-        done
-      '')
+      mpdchck
+      srsh
     ];
     systemd.user.services."mpdchck" = {
       enable = true;
@@ -102,7 +105,12 @@ in {
       after = ["mpd.service"];
       wantedBy = ["default.target"];
       path = [
-        "/run/current-system/sw"
+        mpdchck
+        srsh
+        pipewire
+        wireplumber
+        mpc
+        mpd
       ];
       script = ''mpdchck.sh'';
       serviceConfig = {
