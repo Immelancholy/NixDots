e4be35b12ac323a6d8d9410f903d85866a8cf144
Immelancholy <lenalowes0@gmail.com>
2025-05-03 18:19:48 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -6,16 +6,51 @@
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "mpdart" ''
       source "${inputs.bash-utility}/src/format.sh"
+      TMP=''${TMPDIR:-/tmp}
+      COVER=''${$TMP/albumArt.jpg}
+      MUSIC_DIR="''${MPD_MUSIC_DIR:-~/Music}"
+      PROG_NAME=$(basename "$0")
+
+      show_help() {
+        printf "%s" "\
+      usage: mpdArt [-h|--help] [--music_dir \"path/to/dir\"]
+
+      optional args:
+        -h, --help
+        --music_dir
+      "
+      }
+      options=$(getopt -o h --long 'music_dir:,help' "$@")
+      eval set -- "$options"
+
+      while true; do
+        case "$1" in
+          --music_dir)
+            shift;
+            MUSIC_DIR=$1
+            ;;
+          -h|--help)
+            show_help
+            exit
+            ;;
+        esac
+        shift
+      done
       get_art () {
         song=$(mpc current --format %file%)
         if [ "$song" != "$song_old" ]; then
           clear
-          art=$(playerctl --player=mpd metadata --format "{{mpris:artUrl}}")
           title=$(mpc current --format %title%)
+          DIR="$MUSIC_DIR$(dirname "$(mpc current -f %file%)")"
+          for ART in "$DIR/cover."{png,jpg,webp}; do
+            if [ -f "$ART" ]; then
+              convert "$ART" $COVER &> /dev/null
+            fi
+          done
           artist=$(mpc current --format %artist%)
           line1="-------- $artist --------"
           line2="-------- $title --------"
-          kitten icat --align=center "$art"
+          kitten icat --align=center "$COVER"
           format::text_center "$line1"
           format::text_center "$line2"
           song_old=$song
