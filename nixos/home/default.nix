ba03db2c2dc2aa32cb4dfca0e1c49389fb8bc57a
Immelancholy <lenalowes0@gmail.com>
2025-05-03 14:59:40 +0100

    Update Flake Lock

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -6,30 +6,6 @@
   ...
 }: let
   user = config.home.username;
-  Nakari = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GcHyQ8DWUAAOAho?format=jpg&name=medium";
-    hash = "sha256-YB13taH2jBB6POHniL/muHxCT1Mpir40YgjCkU5Vetc=";
-  };
-
-  Goth_Chell = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GXERYazW0AErHbS?format=jpg&name=medium";
-    hash = "sha256-OX8v3Xh+sDhgVv6P3leWHHZrIPiVuBrnd9iAoEPtFBQ=";
-  };
-
-  Azurenti = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GS9F_-JWMAALb1S?format=jpg&name=medium";
-    hash = "sha256-+IdQuGYwOTUwS/B7tMpDf91o3uerAWaXSs4Wn5/jABU=";
-  };
-
-  Fubuki = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GJDueQOXsAA7DjN?format=jpg&name=medium";
-    hash = "sha256-m7175RBqjgsDb+yNyRkR0AnVp9rvG1nrX7HKpqSJLSU=";
-  };
-
-  Shorea = pkgs.fetchurl {
-    url = "https://pbs.twimg.com/media/GG0t0K6bcAA8-xW?format=jpg&name=medium";
-    hash = "sha256-NJNpjZUxK+6vvOUJMt/NxkaSA1fUqee7czH+z2f/SU4=";
-  };
 in {
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
