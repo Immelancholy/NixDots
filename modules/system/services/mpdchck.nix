79ec2034ccd81363b34ad17966270437968c1f1b
Immelancholy <lenalowes0@gmail.com>
2025-05-03 20:14:06 +0100

    switch

--- modules/system/services/mpdchck.nix
+++ modules/system/services/mpdchck.nix
@@ -20,62 +20,70 @@ with lib; let
   srsh = pkgs.writeShellScriptBin "sr.sh" ''
     pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
     mapfile -t allowedRates < <( pw-metadata -n settings | grep 'clock.allowed-rates' | cut -d "'" -f 4 | tr -d "[" | tr -d "]" | tr -d , )
-    while :
-    do
+    status () {
       state=$(mpc status %state%)
       if [ "$state" != "playing" ];
       then
         pw-metadata -n settings 0 clock.force-rate 0 > /dev/null
 
         break
-      else
-        currentRate=$(mpc status %samplerate%)
+      fi
+    }
+    while :
+    do
+      currentRate=$(mpc status %samplerate%)
 
-        if [ "$currentRate" != "$pwrate" ];
+      status
+      if [ "$currentRate" = "%samplerate%" ];
+      then
+        pw-metadata -n settings 0 clock.force-rate 0 > /dev/null
+
+        break
+      fi
+      if [ "$currentRate" != "$pwrate" ];
+      then
+        if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
         then
-          if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
+          pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
+          pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+
+        elif [ $(( currentRate % 48000 )) -eq 0 ];
+        then
+          if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
           then
-            pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
-            pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+            pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
+            pwrate=''${currentRate}
 
-          elif [ $(( currentRate % 48000 )) -eq 0 ];
+          elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
           then
-            if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
-            then
-              pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
-              pwrate=''${currentRate}
+            pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
+            pwrate=''${currentRate}
 
-            elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
-            then
-              pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
-              pwrate=''${currentRate}
-
-            elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
-            then
-              pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
-              pwrate=''${currentRate}
-
-            else
-              kill "$pid"
-              pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-              pwrate=''${currentRate}
-
-            fi
+          elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
+          then
+            pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
+            pwrate=''${currentRate}
 
           else
-            if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
-            then
-              pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
-              pwrate=''${currentRate}
+            kill "$pid"
+            pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+            pwrate=''${currentRate}
 
-            else
-              pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-              pwrate=''${currentRate}
-
-            fi
           fi
 
+        else
+          if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
+          then
+            pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
+            pwrate=''${currentRate}
+
+          else
+            pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+            pwrate=''${currentRate}
+
+          fi
         fi
+
       fi
 
     done
