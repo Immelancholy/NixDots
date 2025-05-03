fc22e95c48f0dcd02bd4ec2f48f9425a3c0def11
Immelancholy <lenalowes0@gmail.com>
2025-05-03 19:36:23 +0100

    switch

--- modules/system/services/mpdchck.nix
+++ modules/system/services/mpdchck.nix
@@ -43,53 +43,54 @@ in {
             pw-metadata -n settings 0 clock.force-rate 0 > /dev/null
 
             break
-          fi
-          currentRate=$(mpc status %samplerate%)
+          else
+            currentRate=$(mpc status %samplerate%)
 
-          if [ "$currentRate" != "$pwrate" ];
-          then
-            if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
+            if [ "$currentRate" != "$pwrate" ];
             then
-              pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
-              pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
-
-            elif [ $(( currentRate % 48000 )) -eq 0 ];
-            then
-              if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
+              if [[ " ''${allowedRates[*]}" =~ [[:space:]]''${currentRate}[[:space:]] ]];
               then
-                pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
-                pwrate=''${currentRate}
+                pw-metadata -n settings 0 clock.force-rate "$currentRate" > /dev/null
+                pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
 
-              elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
+              elif [ $(( currentRate % 48000 )) -eq 0 ];
               then
-                pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
-                pwrate=''${currentRate}
+                if [[ " ''${allowedRates[*]}" =~ [[:space:]]192000[[:space:]] ]];
+                then
+                  pw-metadata -n settings 0 clock.force-rate 192000 > /dev/null
+                  pwrate=''${currentRate}
 
-              elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
-              then
-                pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
-                pwrate=''${currentRate}
+                elif [[ " ''${allowedRates[*]}" =~ [[:space:]]96000[[:space:]] ]];
+                then
+                  pw-metadata -n settings 0 clock.force-rate 96000 > /dev/null
+                  pwrate=''${currentRate}
+
+                elif [[ " ''${allowedRates[*]}" =~ [[:space:]]48000[[:space:]] ]];
+                then
+                  pw-metadata -n settings 0 clock.force-rate 48000 > /dev/null
+                  pwrate=''${currentRate}
+
+                else
+                  kill "$pid"
+                  pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+                  pwrate=''${currentRate}
+
+                fi
 
               else
-                kill "$pid"
-                pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-                pwrate=''${currentRate}
+                if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
+                then
+                  pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
+                  pwrate=''${currentRate}
 
+                else
+                  pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
+                  pwrate=''${currentRate}
+
+                fi
               fi
 
-            else
-              if [[ " ''${allowedRates[*]}" =~ [[:space:]]88200[[:space:]] ]];
-              then
-                pw-metadata -n settings 0 clock.force-rate 88200 > /dev/null
-                pwrate=''${currentRate}
-
-              else
-                pw-metadata -n settings 0 clock.force-rate 44100 > /dev/null
-                pwrate=''${currentRate}
-
-              fi
             fi
-
           fi
 
         done
