1770a813d1a9e34ca4757eb49a43ec31747b1247
Immelancholy <lenalowes0@gmail.com>
2025-05-05 15:05:12 +0100

    switch

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -39,21 +39,6 @@ in {
       "autostart/input-mapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
         source = "${nixosConfig.services.input-remapper.package}/share/applications/input-remapper-autoload.desktop";
       };
-      "uwsm/env".text = ''
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-        export QT_AUTO_SCREEN_SCALE_FACTOR=1
-        export GDK_SCALE=1
-        export XCURSOR_THEME="${config.stylix.cursor.name}"
-        export XCURSOR_SIZE=${builtins.toString config.stylix.cursor.size}
-        export NIXOS_OZONE_WL=1
-        export QT_QPA_PLATFORM="wayland;xcb"
-        export MOZ_ENABLE_WAYLAND=1
-        export GDK_BACKEND="wayland,x11
-      '';
-      "uwsm/env-hyprland".text = ''
-        export HYPRCURSOR_THEME="${config.stylix.cursor.name}"
-        export HYPRCURSOR_SIZE=${builtins.toString config.stylix.cursor.size}
-      '';
     };
   };
   home.file = {
--- nixos/home/env.nix
+++ nixos/home/env.nix
@@ -23,4 +23,22 @@
     SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
     GNOME_KEYRING_CONTROL = "$XDG_RUNTIME_DIR/keyring";
   };
+
+  xdg.configFile = {
+    "uwsm/env".text = ''
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export QT_AUTO_SCREEN_SCALE_FACTOR=1
+      export GDK_SCALE=1
+      export XCURSOR_THEME="${config.stylix.cursor.name}"
+      export XCURSOR_SIZE=${builtins.toString config.stylix.cursor.size}
+      export NIXOS_OZONE_WL=1
+      export QT_QPA_PLATFORM="wayland;xcb"
+      export MOZ_ENABLE_WAYLAND=1
+      export GDK_BACKEND="wayland,x11
+    '';
+    "uwsm/env-hyprland".text = ''
+      export HYPRCURSOR_THEME="${config.stylix.cursor.name}"
+      export HYPRCURSOR_SIZE=${builtins.toString config.stylix.cursor.size}
+    '';
+  };
 }
