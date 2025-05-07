ca4de344665cdd022ff66ad57ac739429e1b91ce
Immelancholy <lenalowes0@gmail.com>
2025-05-08 00:49:14 +0100

    switch

--- nixos/system/programs/scripts/toggle-mute.nix
+++ nixos/system/programs/scripts/toggle-mute.nix
@@ -5,11 +5,11 @@
     COMMES=$(wpctl status | grep "commes_mic_out" | awk '{print $2}' | grep -m1 "" | cut -f1 -d ".")
     function mute () {
       if [ "$MUTED" = "[MUTED]" ]; then
-        wpctl mute @DEFAULT_SOURCE@ 0
-        wpctl mute "$COMMES" 1
+        wpctl set-mute @DEFAULT_SOURCE@ 0
+        wpctl set-mute "$COMMES" 1
       elif [ "$COMMES" = "" ]; then
-        wpctl mute @DEFAULT_SOURCE@ 1
-        wpctl mute "$COMMES" 0
+        wpctl set-mute @DEFAULT_SOURCE@ 1
+        wpctl set-mute "$COMMES" 0
       fi
     }
     mute
