a9139e102a9e5eab677f912d0352759e1ee35385
Immelancholy <lenalowes0@gmail.com>
2025-05-07 17:37:49 +0100

    switch

--- modules/home/stylix/zenTarget.nix
+++ modules/home/stylix/zenTarget.nix
@@ -49,7 +49,7 @@ in {
           --lwt-sidebar-background-color: ${base} !important;
           --toolbar-bgcolor: ${base} !important;
           --newtab-background-color: ${base} !important;
-          --zen-themed-toolbar-bg: ${base} !important;
+          --zen-themed-toolbar-bg: ${mauve} !important;
           --zen-main-browser-background: ${base} !important;
           --toolbox-bgcolor-inactive: ${base} !important;
         }
