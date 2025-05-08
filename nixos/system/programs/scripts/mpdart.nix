2586393102cfc307fcb57f1afe1219e6115716c2
Immelancholy <lenalowes0@gmail.com>
2025-05-08 22:11:46 +0100

    switch

--- nixos/system/programs/scripts/mpdart.nix
+++ nixos/system/programs/scripts/mpdart.nix
@@ -116,7 +116,8 @@
           line2p1="''${col2}$artist "
           line2p2="''${col3}- "
           line2p3="''${col4}$album"
-          line2p3="''${line2p3:0:50}"
+          cols=$(tput cols)
+          line2p3="''${line2p3:0:$cols}"
           line3="''${col5}─────────────────────────────────╶"
           kitten icat --align=center "$COVER"
           printf '%s\n' "$line1"
