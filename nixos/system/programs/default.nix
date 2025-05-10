340d9f3e515f984c90c5d51aba96c451e83b69ce
Immelancholy <lenalowes0@gmail.com>
2025-05-10 05:01:00 +0100

    switch

--- nixos/system/programs/default.nix
+++ nixos/system/programs/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   inputs,
+  config,
   ...
 }: let
   nrm = inputs.nix-relic-modules.packages.${pkgs.system};
@@ -86,12 +87,37 @@ in {
     nrm.brightness
     nrm.btop
     nrm.cava
-    nrm.cavaCfg
+    (nrm.cavaCfg.override {
+      red = "#${config.lib.stylix.colors.base08}";
+      peach = "#${config.lib.stylix.colors.base09}";
+      yellow = "#${config.lib.stylix.colors.base0A}";
+      green = "#${config.lib.stylix.colors.base0B}";
+      teal = "#${config.lib.stylix.colors.base0C}";
+      blue = "#${config.lib.stylix.colors.base0D}";
+      mauve = "#${config.lib.stylix.colors.base0E}";
+    })
     nrm.checkshell
     nrm.cliphist
     nrm.colortrans
     nrm.mpdchck
-    nrm.neo-color
+    (nrm.neo-color.overrid {
+      base = "${config.lib.stylix.colors.base00}";
+      mantle = "${config.lib.stylix.colors.base01}";
+      surface0 = "${config.lib.stylix.colors.base02}";
+      surface1 = "${config.lib.stylix.colors.base03}";
+      surface2 = "${config.lib.stylix.colors.base04}";
+      text = "${config.lib.stylix.colors.base05}";
+      rosewater = "${config.lib.stylix.colors.base06}";
+      lavender = "${config.lib.stylix.colors.base07}";
+      red = "${config.lib.stylix.colors.base08}";
+      peach = "${config.lib.stylix.colors.base09}";
+      yellow = "${config.lib.stylix.colors.base0A}";
+      green = "${config.lib.stylix.colors.base0B}";
+      teal = "${config.lib.stylix.colors.base0C}";
+      blue = "${config.lib.stylix.colors.base0D}";
+      mauve = "${config.lib.stylix.colors.base0E}";
+      flamingo = "${config.lib.stylix.colors.base0F}";
+    })
     nrm.neo
     nrm.rofi-power-menu
     nrm.ss
