4c65128c81a03a5eb0701e962ede72f8ea25a6d0
Immelancholy <lenalowes0@gmail.com>
2025-05-10 03:43:38 +0100

    switch

--- nixos/home/programs/hyprland/default.nix
+++ nixos/home/programs/hyprland/default.nix
@@ -1,25 +1,37 @@
 {
   pkgs,
   inputs,
+  lib,
+  config,
   ...
-}: {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = null;
-    portalPackage = null;
-    usingFlake = true;
-    xwayland.enable = false;
-    systemd = {
-      # disable the systemd integration, as it conflicts with uwsm.
-      enable = false;
-      variables = ["--all"];
-    };
-    plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
-      inputs.hyprland-plugins.packages.${pkgs.system}.xtra-dispatchers
-      # pkgs.hyprlandPlugins.csgo-vulkan-fix
-    ];
-  };
+}: let
+  cfg = config.wayland.windowManager.hyprland;
+in {
+  wayland.windowManager.hyprland = lib.mkMerge [
+    {
+      enable = true;
+      package = null;
+      portalPackage = null;
+      usingFlake = false;
+      xwayland.enable = false;
+      systemd = {
+        # disable the systemd integration, as it conflicts with uwsm.
+        enable = false;
+        variables = ["--all"];
+      };
+    }
+    (lib.mkIf cfg.usingFlake {
+      plugins = [
+        inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
+        inputs.hyprland-plugins.packages.${pkgs.system}.xtra-dispatchers
+      ];
+    })
+    (lib.mkIf (! cfg.usingFlake) {
+      plugins = [
+        pkgs.hyprlandPlugins.csgo-vulkan-fix
+      ];
+    })
+  ];
   services.hyprpolkitagent.enable = true;
 
   home.packages = with pkgs; [
