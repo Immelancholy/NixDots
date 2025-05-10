99b88c91c92dd7be2d2da197ce377916549dd73a
Immelancholy <lenalowes0@gmail.com>
2025-05-10 02:05:54 +0100

    switch

--- nixos/system/programs/scripts/cavaCfg.nix
+++ nixos/system/programs/scripts/cavaCfg.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   config,
-  writeShellScriptBin,
   ...
 }: let
   base = "#${config.lib.stylix.colors.base00}";
@@ -20,8 +19,8 @@
   blue = "#${config.lib.stylix.colors.base0D}";
   mauve = "#${config.lib.stylix.colors.base0E}";
   flamingo = "#${config.lib.stylix.colors.base0F}";
-in
-  writeShellScriptBin "cavaCfg" ''
+in{
+  pkgs.writeShellScriptBin "cavaCfg" ''
     cavaConfigFile=$HOME/.config/cava/vcConfig
     id=$(${pkgs.wireplumber}/bin/wpctl status | grep "virtual_cable_in" | ${pkgs.gawk}/bin/awk '{print $2}' | grep -m1 "" | cut -f1 -d ".")
     serial=$(${pkgs.wireplumber}/bin/wpctl inspect "''${id}" | sed -n 's/.*object.serial = //p')
@@ -62,4 +61,5 @@ in
     noise_reduction=''${reduce}
     waves=0
     EOF
-  ''
+  '';
+}
--- nixos/system/programs/scripts/default.nix
+++ nixos/system/programs/scripts/default.nix
@@ -1,8 +1,4 @@
 {pkgs, ...}: {
-  # environment.systemPackages = with pkgs; [
-  #   (writeShellScriptBin "waycava.sh" (builtins.readFile ./Bash/waycava.sh))
-  #   (writeShellScriptBin "rofi-power-menu" (builtins.readFile ./Bash/rofi-power-menu))
-  # ];
   imports = [
     ./cavaCfg.nix
     ./neo-color-cfg.nix
