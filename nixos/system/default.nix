b253132d1ad46987c1047c63e4a779579c48982c
Immelancholy <lenalowes0@gmail.com>
2025-05-10 19:16:36 +0100

    switch

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -25,11 +25,11 @@
   services.neo-color = {
     enable = true;
     colors = {
-      color1 = "#${config.lib.stylix.colors.base0F}";
-      color2 = "#${config.lib.stylix.colors.base0D}";
-      color3 = "#${config.lib.stylix.colors.base0E}";
-      color4 = "#${config.lib.stylix.colors.base0C}";
-      color5 = "#${config.lib.stylix.colors.base05}";
+      color1 = "${config.lib.stylix.colors.base0F}";
+      color2 = "${config.lib.stylix.colors.base0D}";
+      color3 = "${config.lib.stylix.colors.base0E}";
+      color4 = "${config.lib.stylix.colors.base0C}";
+      color5 = "${config.lib.stylix.colors.base05}";
     };
   };
 
