4aa3c0fef276256e88d463a41169d7fa4a9536d1
Immelancholy <lenalowes0@gmail.com>
2025-05-11 01:33:23 +0100

    switch

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -17,7 +17,7 @@
   nixpkgs = {
     overlays = [
       outputs.overlays.stable-packages
-      inputs.nix-relic-modules.overlays
+      inputs.nix-relic-modules.overlays.nix-relic-modules
     ];
 
     config = {
