9fdd95bb1e48854368dbad24cf8559f031bdb885
Immelancholy <lenalowes0@gmail.com>
2025-05-14 18:13:39 +0100

    switch

--- nixos/home/oh-my-posh.nix
+++ nixos/home/oh-my-posh.nix
@@ -107,7 +107,7 @@ in {
                     "style": "folder"
                   },
                   "style": "powerline",
-                  "template": "{{if \"~\" .PWD}} \uf015 {{ .Path }} {{if ne \"~\" .PWD}} \udb80\ude4b {{ .Path }}{{ end }} ",
+                  "template": "{{if e \"~\" .PWD}} \uf015 {{ .Path }} {{if ne \"~\" .PWD}} \udb80\ude4b {{ .Path }}{{ end }} ",
                   "type": "path"
                 },
                 {
