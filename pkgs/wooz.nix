134f04c937f6bc2f34cdae0c809c4fb3273a1e42
Immelancholy <lenalowes0@gmail.com>
2025-05-14 23:37:35 +0100

    Woop

--- pkgs/wooz.nix
+++ pkgs/wooz.nix
@@ -1,6 +1,6 @@
 {
   stdenv,
-  fetchFromGithub,
+  fetchFromGitHub,
   pkgs,
   ...
 }:
@@ -8,10 +8,25 @@ stdenv.mkDerivation {
   pname = "wooz";
   version = "0.1.0";
 
-  src = fetchFromGithub {
+  src = fetchFromGitHub {
     owner = "negrel";
     repo = "wooz";
     rev = "9635978be9e8f179ffb03ab0ed4667fc1a93415e";
-    hash = "";
+    hash = "sha256-V+INCnxqrJcv3krGIg1SU1tkazrw/d5iyZC/rdAfLJw=";
   };
+
+  buildInputs = with pkgs; [
+    meson
+    ninja
+  ];
+
+  buildPhase = ''
+    export CFLAGS="-03"
+    meson build
+    ninja -c build install
+  '';
+
+  installPhase = ''
+    cp -r $src/bin $out/bin
+  '';
 }
