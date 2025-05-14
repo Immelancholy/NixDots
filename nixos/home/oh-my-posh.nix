13bde5e9052848f726effbc3f7ab2b54bb388c14
Immelancholy <lenalowes0@gmail.com>
2025-05-14 15:46:41 +0100

    switch

--- nixos/home/oh-my-posh.nix
+++ nixos/home/oh-my-posh.nix
@@ -38,7 +38,7 @@ in {
           },
           "transient_prompt": {
             "background": "transparent",
-            "foreground": "${flamingo}",
+            "foreground": "${mauve}",
             "template": "~~> "
           },
           "blocks": [
