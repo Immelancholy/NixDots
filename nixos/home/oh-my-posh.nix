79921357b893f242a72113220ec9cc19fcfdd29e
Immelancholy <lenalowes0@gmail.com>
2025-05-15 11:48:38 +0100

    switch

--- nixos/home/oh-my-posh.nix
+++ nixos/home/oh-my-posh.nix
@@ -66,7 +66,7 @@ in {
                   "foreground": "p:white",
                   "trailing_diamond": "<transparent,p:plum>\ue0d7</>",
                   "style": "diamond",
-                  "template": " {{ .UserName }} ",
+                  "template": " {{ .UserName }} @",
                   "type": "session"
                 },
                 {
