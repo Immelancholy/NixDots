b89f467520bb2fe928bff55d3e772b07555edb3a
Immelancholy <lenalowes0@gmail.com>
2025-05-15 18:41:16 +0100

    switch

--- hosts/hostHome.nix
+++ hosts/hostHome.nix
@@ -3,24 +3,26 @@
   lib,
   ...
 }:
-with lib; let
-  hostName = config.networking.hostName;
-  makeHM = name: _user: let
-    user = config.users.users.${name};
-  in ({
-    config,
-    pkgs,
-    ...
-  }:
-    recursiveUpdate {
-      _module.args = {
-        inherit hostName user;
-      };
+with lib; {
+  config = let
+    hostName = config.networking.hostName;
+    makeHM = name: _user: let
+      user = config.users.users.${name};
+    in ({
+      config,
+      pkgs,
+      ...
+    }:
+      recursiveUpdate {
+        _module.args = {
+          inherit hostName user;
+        };
 
-      imports = [
-        ./${hostName}/users/${name}/home.nix
-      ];
-    });
-in {
-  home-manager.users = mapAttrs makeHM config.nix-relic.users.users;
+        imports = [
+          ./${hostName}/users/${name}/home.nix
+        ];
+      });
+  in {
+    home-manager.users = mapAttrs makeHM config.nix-relic.users.users;
+  };
 }
