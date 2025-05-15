a45341e55083587a2bfc74ea57c9a556567ae1e6
Immelancholy <lenalowes0@gmail.com>
2025-05-15 16:57:48 +0100

    switch

--- hosts/hostHome.nix
+++ hosts/hostHome.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   nix-relic.home-manager.config = {
     imports = [
-      ./${config.networking.hostName}/users/${config.home.username}/home.nix
+      ./${config.networking.hostName}/users/${config.nix-relic.home-manager.config.home.username}/home.nix
     ];
   };
 }
