c540e47c97aaf03c2ac07a91b8844d46c85c1247
Immelancholy <lenalowes0@gmail.com>
2025-05-16 20:47:40 +0100

    switch

--- nixos/system/env.nix
+++ nixos/system/env.nix
@@ -15,13 +15,13 @@
     ];
   };
 
-  # environment.extraInit =
-  #   /*
-  #   sh
-  #   */
-  #   ''
-  #     if [ -z "$SSH_AUTH_SOCK" -a -n "$XDG_RUNTIME_DIR" ]; then
-  #       export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
-  #     fi
-  #   '';
+  environment.extraInit =
+    /*
+    sh
+    */
+    ''
+      if [ -z "$SSH_AUTH_SOCK" -a -n "$XDG_RUNTIME_DIR" ]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
+      fi
+    '';
 }
