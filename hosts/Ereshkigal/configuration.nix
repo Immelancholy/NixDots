3a7417f2f28570fcecca5fec711fd2e6c490ae74
Immelancholy <lenalowes0@gmail.com>
2025-05-17 02:26:20 +0100

    switch

--- hosts/Ereshkigal/configuration.nix
+++ hosts/Ereshkigal/configuration.nix
@@ -64,7 +64,7 @@
     openFirewall = false;
     settings = {
       PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+      # KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
       MaxStartups = 3;
       AllowTcpForwarding = "no";
