1e0e6fafbb8887e504dcc5f8929f296d54ddc91c
Immelancholy <lenalowes0@gmail.com>
2025-05-22 19:27:16 +0100

    switch

--- nixos/home/programs/ghostty.nix
+++ nixos/home/programs/ghostty.nix
@@ -12,7 +12,11 @@
       cursor-click-to-move = true;
       window-padding-balance = true;
       window-padding-x = 5;
+      window-padding-y = 5;
       background-opacity = 0.80;
+      desktop-notifications = true;
+      gtk-gsk-renderer = "default";
+      window-padding-color = "background";
     };
   };
 }
