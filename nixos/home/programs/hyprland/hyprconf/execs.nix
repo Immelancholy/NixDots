5351963ed259d9e6cde52fd7b6ad1c0279f32047
Immelancholy <lenalowes0@gmail.com>
2025-05-22 17:39:32 +0100

    switch

--- nixos/home/programs/hyprland/hyprconf/execs.nix
+++ nixos/home/programs/hyprland/hyprconf/execs.nix
@@ -4,9 +4,9 @@
       # "hyprctl setcursor ${config.stylix.cursor.name} ${builtins.toString config.stylix.cursor.size}"
       "systemctl restart --user nm-applet"
       "sleep 10; systemctl restart --user remmina"
-      ''[workspace 1 silent; float; size 858 462; move 640 609] $term --class "cava" cava.sh''
-      ''[workspace 1 silent; float; size 620 637; move 10 433] $term --class "btop" btop.sh''
-      ''[workspace 1 silent; float; size 402 1030; move 1508 42] $term --class "neo" neo.sh''
+      ''[workspace 1 silent; float; size 858 462; move 640 609] $term cava.sh''
+      ''[workspace 1 silent; float; size 620 637; move 10 433] $term btop.sh''
+      ''[workspace 1 silent; float; size 402 1030; move 1508 42] $term neo.sh''
       ''[workspace 1 silent; float; size 620 383; move 10 42] env class="fastfetch" $term''
       "[workspace 2 silent] $browser"
       "[workspace 2 silent] uwsm app -- teams-for-linux --minimized"
