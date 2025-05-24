5af6da307a99916abaff12e9f5c436e0ec8270c4
Immelancholy <lenalowes0@gmail.com>
2025-05-24 12:09:59 +0100

    switch

--- nixos/home/programs/hyprland/default.nix
+++ nixos/home/programs/hyprland/default.nix
@@ -53,7 +53,7 @@ in {
         tmux kill-session -t "Music"
           hyprctl dispatch exec '[workspace 1 silent; float; size 858 462; move 640 609] uwsm app -- ghostty --window-padding-x=0 --window-padding-y=0 --font-size=4 --class=cava.ghostty.screen1 -e cava -p "$XDG_CONFIG_HOME"/cava/vcConfig'
           hyprctl dispatch exec '[workspace 1 silent; float; size 620 637; move 10 433] uwsm app -- ghostty --font-size=9 --class=btop.ghostty.screen1 -e btop'
-          hyprctl dispatch exec '[workspace 1 silent; float; size 402 1030; move 1508 42]  uwsm app -- ghostty --window-padding-x=0 --window-padding-y=0 --class=neo.ghostty.screen1 -e neo -a -S 20 -d 1 -f 144 -C "$XDG_CONFIG_HOME"/neo/colors -b 1 -m "Welcome, ''${USER^}." --lingerms=1,1 --rippct=0'
+          hyprctl dispatch exec '[workspace 1 silent; float; size 402 1030; move 1508 42]  uwsm app -- ghostty --window-padding-x=0 --window-padding-y=0 --class=neo.ghostty.screen1 -e neo.sh'
           hyprctl dispatch exec '[workspace 1 silent; float; size 620 383; move 10 42] env class="fastfetch" uwsm app -- ghostty --class=fastfetch.ghostty.screen1'
           hyprctl dispatch exec '[workspace 1 silent; float; size 858 559; move 640 42] ${playerCmd}'
       '';
