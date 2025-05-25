6810ae2f97fc33693c9c966dfedf1ae0d7115c78
Immelancholy <lenalowes0@gmail.com>
2025-05-25 14:46:37 +0100

    switch

--- nixos/home/programs/ghostty.nix
+++ nixos/home/programs/ghostty.nix
@@ -5,7 +5,7 @@
 }: {
   programs.ghostty = {
     enable = true;
-    # package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    package = inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
     installVimSyntax = true;
     settings = {
       font-synthetic-style = false;
