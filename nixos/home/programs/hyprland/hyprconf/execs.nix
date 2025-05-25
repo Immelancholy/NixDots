c171a6a9fff097a1ba119326a4aa4d3cf042b0ea
Immelancholy <lenalowes0@gmail.com>
2025-05-25 15:03:45 +0100

    switch

--- nixos/home/programs/hyprland/hyprconf/execs.nix
+++ nixos/home/programs/hyprland/hyprconf/execs.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       # "hyprctl setcursor ${config.stylix.cursor.name} ${builtins.toString config.stylix.cursor.size}"
-      "/home/mela/systemd-restarts.sh"
+      "systemd-restarts"
       ''[workspace 1 silent; float; size 858 462; move 640 609] $term --window-padding-x=0 --window-padding-y=0 --font-size=4 --class=cava.ghostty.screen1 -e cava -p "$XDG_CONFIG_HOME"/cava/vcConfig''
       ''[workspace 1 silent; float; size 620 637; move 10 433] $term --font-size=9 --class=btop.ghostty.screen1 -e btop''
       ''[workspace 1 silent; float; size 402 1030; move 1508 42]  $term --window-padding-x=0 --window-padding-y=0 --class=neo.ghostty.screen1 -e neo.sh''
