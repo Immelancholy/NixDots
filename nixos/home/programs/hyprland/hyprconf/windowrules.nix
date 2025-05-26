e3e1cac90dafdc8481ee000ac9b37bae4196cba0
Immelancholy <lenalowes0@gmail.com>
2025-05-26 21:51:46 +0100

    Update zen windowrules

--- nixos/home/programs/hyprland/hyprconf/windowrules.nix
+++ nixos/home/programs/hyprland/hyprconf/windowrules.nix
@@ -8,7 +8,7 @@
       ''opacity 1.0 0.90,class:^(fastfetch)$''
       ''opacity 1.0 0.90,class:^(kitty)$''
       ''opacity 1.0 0.90,title:^(ghostty)$''
-      ''opacity 1.0 0.90,class:^(zen)$''
+      ''opacity 1.0 0.70,class:^(zen)$''
       ''opacity 0.80 0.70,class:^(gcr-prompter)$''
       ''opacity 0.80 0.70,class:^(org.gnome.seahorse.Application)$''
       ''opacity 0.80 0.70,class:^(org.prismlauncher.PrismLauncher)$''
