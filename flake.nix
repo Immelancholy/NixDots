74730dcba41feb35dc6d9ed541f16dfd3cb6a39b
Immelancholy <lenalowes0@gmail.com>
2025-07-04 17:37:28 +0100

    chore: update llm-git-commit flake input
    Changes the flake input URL for the llm-git-commit repository to point to the main branch.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     llm-git-commit = {
-      url = "github:Immelancholy/llm-git-commit/add-master-branch-option-to-flake";
+      url = "github:ShamanicArts/llm-git-commit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-relic-modules = {
