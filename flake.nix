ef2a10268c1e57b122a43e1e23cfc1770d9ed9cf
Immelancholy <lenalowes0@gmail.com>
2025-07-20 10:48:46 +0100

    chore: update Solaar-Flake input to use GitHub repo
    Switches the Solaar-Flake input in `flake.nix` and `flake.lock` from a tarball URL to a GitHub repository reference. This ensures the flake uses the latest unstable version directly from the main branch.

--- flake.nix
+++ flake.nix
@@ -53,9 +53,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     solaar = {
-      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      # url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
-      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
       inputs.nixpkgs.follows = "nixpkgs";
     };
     yazi.url = "github:sxyazi/yazi";
