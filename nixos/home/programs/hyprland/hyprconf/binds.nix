4a83830f227a82f95394f518fe1dfcc13c72af8f
Immelancholy <lenalowes0@gmail.com>
2025-10-13 01:49:48 +0100

    chore: Update hyprquickshot and simplify screenshot binds
    Updates the `hyprquickshot` dependency version.
    Removes outdated screenshot submap bindings and replaces them with a single binding to `hyprquickshot -n` for new window screenshots.

--- nixos/home/programs/hyprland/hyprconf/binds.nix
+++ nixos/home/programs/hyprland/hyprconf/binds.nix
@@ -37,9 +37,7 @@
         ''Ctrl+Shift, Escape, exec, uwsm app -- $term --title "btop" btop''
         ", XF86Calculator, exec, uwsm-app -- qalculate-gtk"
         "$mod, X, togglespecialworkspace"
-        ", Print, exec, uwsm-app -- ss.sh -o"
-        "Ctrl, Print, exec, uwsm-app -- ss.sh -f"
-        "Ctrl+Shift, Print, exec, uwsm-app -- ss.sh -w"
+        ", Print, exec, quickshell -c hyprquickshot -n"
         "$mod, O, exec, uwsm-app -- hyprpicker -a"
       ]
       ++ (
@@ -77,15 +75,6 @@
     bindel  = Shift, I, exec, $playerctl previous # next
     bind = , escape, submap, reset
     bind = $mod, P, submap, reset
-    submap = reset
-
-    bind = $mod, P, submap, ss
-    submap = ss
-    bind = , O, exec, uwsm-app -- ss.sh -o
-    bind = , A, exec, uwsm-app -- ss.sh -af
-    bind = , W, exec, uwsm-app -- ss.sh -w
-    bind = , escape, submap, reset
-    bind = , P, submap, reset
     submap = reset
   '';
 }
