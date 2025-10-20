fc4c40cadb421eed1cfcd358fb117119d5613eef
Immelancholy <lenalowes0@gmail.com>
2025-10-20 21:36:32 +0100

    chore: update flake.lock and add cava package
    Updates the Nix flake lock file and adds the cava package to the home manager configuration.

--- nixos/home/programs/cava/default.nix
+++ nixos/home/programs/cava/default.nix
@@ -1,28 +1,5 @@
-{
-  programs.cava = {
-    enable = true;
-    settings = {
-      general = {
-        framerate = 144;
-        sensitivity = 100;
-        bars = 0;
-        bar_width = 1;
-        bar_spacing = 0;
-      };
-      output = {
-        method = "noncurses";
-        channels = "stereo";
-        mono_option = "average";
-      };
-      input = {
-        method = "fifo";
-        source = "/tmp/mpd_cava.fifo";
-      };
-      smoothing = {
-        monstercat = 1;
-        waves = 0;
-        noise_reduction = 72;
-      };
-    };
-  };
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    cava
+  ];
 }
