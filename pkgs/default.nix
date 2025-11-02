e5cdbaefc11862d2c33a05ac0773dd02cd2b9e8f
Immelancholy <lenalowes0@gmail.com>
2025-11-02 05:45:54 +0000

    chore: add winboat package and include in Enlil system
    - Defines the 'winboat' package in pkgs/default.nix.
    - Adds 'winboat' to the systemPackages for the 'Enlil' host.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,4 @@ pkgs: {
   wooz = pkgs.callPackage ./wooz.nix {};
   bongo-cat = pkgs.callPackage ./bongo-cat.nix {};
   cavasik = pkgs.callPackage ./cavasik.nix {};
-  winboat = pkgs.callPackage ./winboat.nix {};
 }
