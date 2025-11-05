7da443c9814706dd915c182d3f21c79f5a2e78e7
Immelancholy <lenalowes0@gmail.com>
2025-11-05 18:48:43 +0000

    feat(home): Configure MPV with HDR support and scripts
    This change enables support for HDR (High Dynamic Range) video playback in MPV by overriding the package with extra arguments. It also integrates several useful MPV scripts for enhanced playback experience and configures MPV to use Vulkan for hardware decoding in a Wayland environment.
    
    - Enables HDR WSI by default for MPV.
    - Installs `mpris`, `modernz`, and `thumbfast` MPV scripts.
    - Sets MPV profile to `gpu-hq` with Vulkan GPU API.
    - Configures MPV to use `gpu-next` video output with a Wayland Vulkan context.
    - Enables `target-colorspace-hint` for accurate color rendering.

--- hosts/Enlil/users/mela/home.nix
+++ hosts/Enlil/users/mela/home.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   imports = [
@@ -11,6 +12,32 @@
     targets = {
       blender.enable = true;
     };
+  };
+
+  programs.mpv = {
+    package = pkgs.mpv.override {
+      extraMakeWrapperArgs = [
+        "--prefix"
+        "ENABLE_HDR_WSI"
+        "="
+        "1"
+      ];
+      scripts = [
+        pkgs.mpvScripts.mpris
+        pkgs.mpvScripts.modernz
+        pkgs.mpvScripts.thumbfast
+      ];
+    };
+    config = {
+      profile = "gpu-hq";
+      gpu-api = "vulkan";
+      osc = "no";
+      hwdec = "auto";
+      vo = "gpu-next";
+      gpu-context = "waylandvk";
+      target-colorspace-hint = true;
+    };
+    scripts = lib.mkForce [];
   };
 
   programs.ssh = {
