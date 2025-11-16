0123026d6ac0b3a48dbaaf05c73c6df7e166d20c
Immelancholy <lenalowes0@gmail.com>
2025-11-16 22:42:25 +0000

    style: remove unused obs-studio plugins
    Removes `droidcam-obs` and `obs-pipewire-audio-capture` as they are no longer needed for Mela's OBS configuration.

--- hosts/Enlil/users/mela/home.nix
+++ hosts/Enlil/users/mela/home.nix
@@ -84,12 +84,8 @@
 
   programs.obs-studio = {
     enable = true;
-    package = pkgs.obs-studio;
-
     plugins = with pkgs.obs-studio-plugins; [
       obs-vkcapture
-      droidcam-obs
-      obs-pipewire-audio-capture
     ];
   };
 
