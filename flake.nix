88c11c1b41a5950eeea4fae3e7d8a1a57c844366
Immelancholy <lenalowes0@gmail.com>
2025-12-08 01:05:13 +0000

    fix: remove extra Nix substituters and trusted public keys
    Removes `zed.cachix.org` and `cache.garnix.io` from extra-substituters and
    extra-trusted-public-keys in flake.nix.

--- flake.nix
+++ flake.nix
@@ -52,18 +52,6 @@
           nix-relic.nixosModules.default
           inputs.disko.nixosModules.default
           ./hosts/${host}
-          {
-            nix.settings = {
-              extra-substituters = [
-                "https://zed.cachix.org"
-                "https://cache.garnix.io"
-              ];
-              extra-trusted-public-keys = [
-                "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
-                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-              ];
-            };
-          }
           (
             {
               config,
