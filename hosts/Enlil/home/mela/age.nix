4062f0ed32729315cc5fd5e694bc7255bc75e368
Immelancholy <lenalowes0@gmail.com>
2025-12-20 18:10:23 +0000

    refactor: remove context7-zed secret from Enlil
    Removes the `context7-zed` age secret file and clears its
    reference from the home configuration. Also applies minor
    formatting to `identityPaths`.

--- hosts/Enlil/home/mela/age.nix
+++ hosts/Enlil/home/mela/age.nix
@@ -1,6 +1,8 @@
 {
   age = {
-    identityPaths = ["/home/mela/.ssh/id_ed25519"];
+    identityPaths = [
+      "/home/mela/.ssh/id_ed25519"
+    ];
     secrets = {
       context7-zed.file = ../../secrets/context7-zed.age;
     };
