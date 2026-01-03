cdd34546ba713fec61cde94a2d010f1ebd50cd26
Immelancholy <lenalowes0@gmail.com>
2026-01-03 18:12:49 +0000

    refactor: enhance application and audio configs
    - pipewire: add WirePlumber rule to force high quality LDAC for Bluetooth.
    - steam: override package with optimized environment variables (MangoHud, GameMode, Proton-Wayland, and LDAC).

--- hosts/Enlil/programs/pipewire.nix
+++ hosts/Enlil/programs/pipewire.nix
@@ -1,11 +1,31 @@
 {
-  services.pipewire.extraConfig.pipewire."92-low-latency" = {
-    "context.properties" = {
-      "default.clock.allowed-rates" = [44100 48000 88200 96000];
-      "default.clock.min-quantum" = 64;
-      "default.clock.max-quantum" = 512;
-      "default.clock.quantum-limit" = 4096;
-      "default.clock.quantum-floor" = 32;
+  services.pipewire = {
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.allowed-rates" = [44100 48000 88200 96000];
+        "default.clock.min-quantum" = 64;
+        "default.clock.max-quantum" = 512;
+        "default.clock.quantum-limit" = 4096;
+        "default.clock.quantum-floor" = 32;
+      };
+    };
+    wireplumber.extraConfig = {
+      "51-bluez-config" = {
+        "monitor.bluez.rules" = [
+          {
+            matches = [
+              {
+                "device.name" = "~bluez_card";
+              }
+            ];
+            actions = {
+              update-props = {
+                "bluez5.a2dp.ldac.quality" = "hq";
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
--- hosts/Enlil/programs/steam.nix
+++ hosts/Enlil/programs/steam.nix
@@ -1,6 +1,22 @@
 {
+  pkgs,
+  lib,
+  ...
+}: let
+  steam_pkg = pkgs.steam.override {
+    extraEnv = {
+      OBS_VKCAPTURE = true;
+      MANGOHUD = true;
+      DXVK_HUD = "compiler";
+      LD_PRELOAD = "${pkgs.gamemode.lib}/lib/libgamemode.so";
+      PROTON_ENABLE_WAYLAND = 1;
+      PROTON_USE_NTSYNC = 1;
+    };
+  };
+in {
   programs.steam = {
     enable = true;
+    package = lib.mkDefault steam_pkg;
     gamescopeSession = {
       enable = true;
     };
