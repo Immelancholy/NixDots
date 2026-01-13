cd1a2b5485d5869adf8c2174cec1735b964fdf89
Immelancholy <lenalowes0@gmail.com>
2026-01-13 23:21:40 +0000

    refactor(hyprland): update binds and system settings
    - Enable hyprexpo and disable hyprspace.
    - Add mouse binds for media control and volume.
    - Implement toggle-mute keybindings.
    - Enable tearing and enforce ecosystem permissions.
    - Set compose key to right win.

--- hosts/Ereshkigal/home/mela/windowManagers/hyprland.nix
+++ hosts/Ereshkigal/home/mela/windowManagers/hyprland.nix
@@ -1,4 +1,4 @@
-{nix-relic, ...}: {
+{
   wayland.windowManager.hyprland = {
     # ONLY ENABLE 1 LAYOUT!!
     layout = {
@@ -6,13 +6,22 @@
       dwindle.enable = false;
       hy3.enable = true;
     };
+    hyprspace.enable = false;
+    hyprexpo.enable = true;
     settings = {
+      ecosystem = {
+        enforce_permissions = true;
+      };
+      general = {
+        allow_tearing = true;
+      };
       cursor = {
-        no_hardware_cursors = false;
+        no_hardware_cursors = 2;
       };
       monitor = "eDP-1, 1920x1080@60, 0x0, 1";
       input = {
         kb_layout = "gb";
+        kb_options = "compose:rwin";
         follow_mouse = "1";
 
         sensitivity = "0";
@@ -20,9 +29,24 @@
         numlock_by_default = "true";
       };
       bind = [
-        # "$mod, F9, pass, class:^(com.obsproject.Studio)$"
-        # "$mod, F10, pass, class:^(com.obsproject.Studio)$"
-        # "$mod, F12, pass, class:^(com.obsproject.Studio)$"
+        "$mod, F9, pass, class:^(com.obsproject.Studio)$"
+        "$mod, F10, pass, class:^(com.obsproject.Studio)$"
+        "$mod, F12, pass, class:^(com.obsproject.Studio)$"
+      ];
+      bindl = [
+        "$mod, mouse:282, exec, $playerctl next" #next
+        "$mod, mouse:281, exec, $playerctl previous" #previous,
+        ", mouse:277, exec, $playerctl play-pause" # toggle between media play and pause
+      ];
+      bindel = [
+        ", mouse:281, exec, uwsm-app -- playerVol inc" # volume up of mpd
+        ", mouse:282, exec, uwsm-app -- playerVol dec" # volume down of mpd
+      ];
+      bindnt = [
+        ", mouse:275, exec, toggle-mute"
+        "Shift, mouse:275, exec, toggle-mute --mute-all"
+        "$mod, M, exec, toggle-mute"
+        "$mod+Shift, M, exec, toggle-mute --mute-all"
       ];
     };
   };
