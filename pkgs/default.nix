30fa9e3c1644bdc373da4aa311bfd9c569b92a2b
Immelancholy <lenalowes0@gmail.com>
2026-03-23 20:43:43 +0000

    refactor: remove easyeffects and gamemode override
    - Delete custom easyeffects package and associated patch.
    - Remove gamemode source override from overlays.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 pkgs: {
   autoeq-fiio = pkgs.callPackage ./autoeq-fiio.nix {};
-  easyeffects = pkgs.callPackage ./easyeffects/package.nix {};
   keychron-udev-rules = pkgs.callPackage ./keychron-udev-rules.nix {};
   toggle-exit-node = pkgs.callPackage ./toggle-exit-node.nix {};
 }
