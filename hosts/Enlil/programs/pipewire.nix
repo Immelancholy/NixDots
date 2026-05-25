c66c8f78b8f14befe0a43f7c5ab6ca39fd3d5a71
Immelancholy <lenalowes0@gmail.com>
2026-05-25 14:38:59 +0100

    chore(nix): update PipeWire configuration and patchbay
    - Increase default clock sample rates to include 192kHz in PipeWire.
    - Remap audio routing in `Default.qpwgraph` to support Scarlett 2i2 hardware.

--- hosts/Enlil/programs/pipewire.nix
+++ hosts/Enlil/programs/pipewire.nix
@@ -2,7 +2,7 @@
   services.pipewire = {
     extraConfig.pipewire."92-low-latency" = {
       "context.properties" = {
-        "default.clock.allowed-rates" = [44100 48000 88200 96000];
+        "default.clock.allowed-rates" = [44100 48000 88200 96000 192000];
         "default.clock.min-quantum" = 64;
         "default.clock.max-quantum" = 256;
         "default.clock.quantum-limit" = 512;
