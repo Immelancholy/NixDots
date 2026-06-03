62b0e5cd0e2477e786e66fc11152f312b2c0af64
Immelancholy <lenalowes0@gmail.com>
2026-06-03 19:09:58 +0100

    fix: correct syntax and update audio gain
    - Fix parenthesis and comma placement in autostart configuration.
    - Increase input gain for Rode-NT1 microphone configuration.

--- hosts/Enlil/home/mela/windowManagers/lua/autostart.nix
+++ hosts/Enlil/home/mela/windowManagers/lua/autostart.nix
@@ -6,12 +6,12 @@
         hl.exec_cmd(Browser, { workspace = "2 silent" })
       end, { timeout = 1, type = "oneshot" })
     end)
-    hl.on("config.reloaded"), function()
+    hl.on("config.reloaded", function()
       hl.timer(function()
         hl.exec_cmd("systemctl restart --user easyeffects")
         hl.exec_cmd("systemctl restart --user qpwgraph")
         hl.exec_cmd("systemctl restart --user waybar")
-      end, {timeout = 1, type = "oneshot"})
+      end, { timeout = 1, type = "oneshot" })
     end)
   '';
 }
