2f1c13574c2fe461da89e091f37a3f57f5b5eb16
Immelancholy <lenalowes0@gmail.com>
2026-06-06 21:55:07 +0100

    pre-commit: add stylua

--- flake.nix
+++ flake.nix
@@ -172,6 +172,17 @@
             src = ./.;
             hooks = {
               nixfmt.enable = true;
+
+              stylua = {
+                enable = true;
+                args = [
+                  "--indent-type"
+                  "Spaces"
+                  "--indent-width"
+                  "2"
+                  "-"
+                ];
+              };
             };
 
             package = pkgs.prek;
