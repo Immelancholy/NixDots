e9e702604c10a76d67b2e27bcd6668ad63c42dba
Immelancholy <lenalowes0@gmail.com>
2026-06-14 15:51:31 +0100

    Pipewire: Setup JACK settings

--- hosts/Enlil/programs/pipewire.nix
+++ hosts/Enlil/programs/pipewire.nix
@@ -1,18 +1,29 @@
 {
   services.pipewire = {
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.allowed-rates" = [
-          44100
-          48000
-          88200
-          96000
-          192000
-        ];
-        "default.clock.min-quantum" = 64;
-        "default.clock.max-quantum" = 256;
-        "default.clock.quantum-limit" = 512;
-        "default.clock.quantum-floor" = 64;
+    extraConfig = {
+      pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            192000
+          ];
+          "default.clock.min-quantum" = 64;
+          "default.clock.max-quantum" = 256;
+          "default.clock.quantum-limit" = 512;
+          "default.clock.quantum-floor" = 64;
+        };
+      };
+      jack."92-low-latency" = {
+        "jack.properties" = {
+          "rt.prio" = 88;
+          "node.latency" = "512/192000";
+          "node.rate" = "1/192000";
+          "node.quantum" = "512/192000";
+          "node.force-quantum" = 512;
+        };
       };
     };
     wireplumber.extraConfig = {
