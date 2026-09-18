17b5c6127c11e2cf83e4c5ca7d6d1f58b0b13d63
Immelancholy <lenalowes0@gmail.com>
2026-09-18 22:52:23 +0100

    fix nvidia

--- hosts/Enlil/drivers/graphics.nix
+++ hosts/Enlil/drivers/graphics.nix
@@ -1,7 +1,28 @@
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   hardware.nvidia = {
     enable = true;
     open = true;
-    branch = "bleeding_edge";
+    package =
+      let
+        base = (pkgs.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.latest;
+      in
+      base.overrideAttrs (old: {
+        passthru = old.passthru // {
+          open = old.passthru.open.overrideAttrs (o: {
+            patches = (o.patches or [ ]) ++ [ ./nvidia-open-gpio-device-const.patch ];
+          });
+        };
+      });
   };
+
+  nix.settings.substituters = lib.mkAfter [ "https://cache.nixos-cuda.org" ];
+  nix.settings.trusted-public-keys = lib.mkAfter [
+    "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+  ];
 }
